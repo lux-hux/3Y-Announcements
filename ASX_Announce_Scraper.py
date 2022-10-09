ed8f224927bd416254f29b489a0def40589d6c6f
lux-hux <100098478+lux-hux@users.noreply.github.com>
2022-10-09 22:16:01 +1100

    cleaned

--- ASX_Announce_Scraper.py
+++ ASX_Announce_Scraper.py
@@ -215,22 +215,7 @@ class ASX_Announcement_Scrape():
         
             shutil.move(link_no + '-' + ticker + ".pdf", str(self.today.year) + '-' + str(self.today.month) + '-' + str(self.today.day) + '-3Y Announcements')      
 
-    
-url = "https://www.asx.com.au/asx/v2/statistics/todayAnns.do"
 
-scrape = ASX_Announcement_Scrape(url)
-
-df_returned = scrape.find_announcements() 
-
-print(df_returned.head())
-
-df_cleaned = scrape.clean_df(df_returned)
-
-df_dir = scrape.isolate_dir(df_cleaned)
-
-scrape.launch_webdriver()
-
-scrape.scrape_links(df_dir)
 
 
 
